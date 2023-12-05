3b342f54a8a470337d98c1e6c93995535beefd59
Hunter Hamilton <iloveyouexe1@gmail.com>
2023-12-05 00:04:33 -0600

    Test complete, still setting up Program.Cs file

--- AdventOfCode/Days/DayOne.cs
+++ AdventOfCode/Days/DayOne.cs
@@ -1,36 +1,33 @@
 ﻿using System;
-using System.IO;
 
 namespace AdventOfCode.Days
 {
     public class DayOne
     {
-        static void Main()
+        public static int CalculateCalibrationSum(string[] lines)
         {
-            //Read the puzzle input from a file
-            string[] lines = File.ReadAllLines("input.txt");
-
             int sum = 0;
 
             foreach (string line in lines)
             {
-                //Check if the line is not empty
+                // Check if the line is not empty
                 if (!string.IsNullOrWhiteSpace(line))
                 {
-                    //Extracting the first and last characters as digits
+                    // Extracting the first and last characters as digits
                     char firstChar = line[0];
                     char lastChar = line[line.Length - 1];
 
-                    //Convert characters to integers
+                    // Convert characters to integers
                     int firstDigit = int.Parse(firstChar.ToString());
                     int lastDigit = int.Parse(lastChar.ToString());
 
-                    //Calculate the the calibration value and add it to the sum
+                    // Calculate the calibration value and add it to the sum
                     int calibrationValue = (firstDigit * 10) + lastDigit;
                     sum += calibrationValue;
                 }
             }
-            Console.WriteLine($"Sum of calibration values: {sum}");
+
+            return sum;
         }
     }
 }
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Choose a day to run:");
+            Console.WriteLine("1. Day One");
+            //Console.WriteLine("2. Day Two");
+            // Add more options for other days
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    // Call DayOne logic here
+                    break;
+                case 2:
+                    // Call DayTwo logic here
+                    break;
+                // Add cases for other days
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+}
