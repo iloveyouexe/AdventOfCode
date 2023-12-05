bdd6b3eb876305c14cbd55914c3d7ce07b37d264
Hunter Hamilton <iloveyouexe1@gmail.com>
2023-12-05 00:15:24 -0600

    Oh man

---
+++ AdventOfCodeApp/Days/DayOne.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AdventOfCode.Days
+{
+    public class DayOne
+    {
+        public static int CalculateCalibrationSum(string[] lines)
+        {
+            int sum = 0;
+
+            foreach (string line in lines)
+            {
+                // Check if the line is not empty
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    // Extracting the first and last characters as digits
+                    char firstChar = line[0];
+                    char lastChar = line[line.Length - 1];
+
+                    // Convert characters to integers
+                    int firstDigit = int.Parse(firstChar.ToString());
+                    int lastDigit = int.Parse(lastChar.ToString());
+
+                    // Calculate the calibration value and add it to the sum
+                    int calibrationValue = (firstDigit * 10) + lastDigit;
+                    sum += calibrationValue;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ AdventOfCodeApp/DaysTests/DayOneTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using AdventOfCode.Days; // Importing DayOne class namespace
+
+namespace AdventOfCode.DaysTests
+{
+    public class DayOneTests
+    {
+        [Test]
+        public void TestCalibrationSum()
+        {
+            // Arrange
+            string[] testInput = { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" };
+            int expectedSum = 142;
+
+            // Act
+            int actualSum = DayOne.CalculateCalibrationSum(testInput);
+
+            // Assert
+            Assert.Equals(expectedSum, actualSum);
+        }
+    }
+}
