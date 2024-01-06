1e3048854549b9e8268f3aa5cf07015607ebd17f
iloveyou.exe <iloveyouexe1@gmail.com>
2024-01-05 21:16:16 -0600

    fixed program.cs

--- AdventOfCodeApp/Program.cs
+++ AdventOfCodeApp/Program.cs
@@ -9,8 +9,12 @@ namespace AdventOfCode
     {
         private static void Main()
         {
-            var input = LoadDayInput("../../../Year2018/Day01/day1.txt");
-            var result = Year2018.Day01.Day1A.Solve(input);
+            var input = LoadDayInput("Year2016/Day12/day12.txt");
+            var result = Day12A.Solve(input);
+            Console.WriteLine($"Day 12A Result (Year 2016): {result}");
+            
+            input = LoadDayInput("../../../Year2018/Day01/day1.txt");
+            result = Year2018.Day01.Day1A.Solve(input);
             Console.WriteLine($"Day 1A Result (Year2018): {result}");
             
             input = LoadDayInput("../../../Year2018/Day02/day2.txt");
@@ -22,12 +26,9 @@ namespace AdventOfCode
             Console.WriteLine($"Day 2B Result (Year2018): {result}");
             
             input = LoadDayInput("Year2023/Day01/day1.txt");
-            result = Day1A.Solve(input);
             Console.WriteLine($"Day 1A Result (Year 2023): {result}");
+            result = Day1A.Solve(input);
             
-            input = LoadDayInput("Year2016/Day12/day12.txt");
-            result = Day12A.Solve(input);
-            Console.WriteLine($"Day 12A Result (Year 2016): {result}");
   
             // Call Day2A from Year2023 namespace
             // input = LoadDayInput("Year2023/Day02/day2.txt");
