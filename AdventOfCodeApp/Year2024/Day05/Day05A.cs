3867c9c0c5a9b4187e1642a4d462482a9435804d
iloveyouexe <bmo@bmo.rip>
2024-12-20 12:38:43 -0600

    CAN I CATCH UP?

--- AdventOfCodeApp/Year2024/Day05/Day05A.cs
+++ AdventOfCodeApp/Year2024/Day05/Day05A.cs
@@ -1,77 +1,94 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-namespace AdventOfCode.Year2024.Day05;
 
-public class Day05A
+namespace AdventOfCode.Year2024.Day05
 {
-    public static string Solve(string[] lines)
+    public class Day05A 
     {
-        string[] input = System.IO.File.ReadAllLines("input.txt");
-        var (rules, updates) = ParseInput(input);
-        int sum = SumMiddlePages(rules, updates);
-        return sum.ToString();
-    }
-
-    static (Dictionary<int, HashSet<int>>, List<List<int>>) ParseInput(string[] input)
-    {
-        var rules = new Dictionary<int, HashSet<int>>();
-        var updates = new List<List<int>>();
-        bool parsingRules = true;
-
-        foreach (var line in input)
+        public string Solve(string[] lines)
         {
-            if (string.IsNullOrWhiteSpace(line))
-            {
-                parsingRules = false;
-                continue;
-            }
-
-            if (parsingRules)
-            {
-                var parts = line.Split('|');
-                int before = int.Parse(parts[0]);
-                int after = int.Parse(parts[1]);
-                if (!rules.ContainsKey(before)) rules[before] = new HashSet<int>();
-                rules[before].Add(after);
-            }
-            else
-            {
-                updates.Add(line.Split(',').Select(int.Parse).ToList());
-            }
+            var (rules, updates) = ParseInput(lines);
+            int sum = SumMiddlePages(rules, updates);
+            return sum.ToString();
         }
 
-        return (rules, updates);
-    }
-
-    static int SumMiddlePages(Dictionary<int, HashSet<int>> rules, List<List<int>> updates)
-    {
-        int sum = 0;
-
-        foreach (var update in updates)
+        private static (Dictionary<int, HashSet<int>>, List<List<int>>) ParseInput(string[] input)
         {
-            if (IsCorrectOrder(rules, update))
-            {
-                int middleIndex = update.Count / 2;
-                sum += update[middleIndex];
-            }
-        }
+            var rules = new Dictionary<int, HashSet<int>>();
+            var updates = new List<List<int>>();
+            bool parsingRules = true;
 
-        return sum;
-    }
-
-    static bool IsCorrectOrder(Dictionary<int, HashSet<int>> rules, List<int> update)
-    {
-        for (int i = 0; i < update.Count; i++)
-        {
-            for (int j = i + 1; j < update.Count; j++)
+            foreach (var line in input)
             {
-                if (rules.ContainsKey(update[j]) && rules[update[j]].Contains(update[i]))
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                if (parsingRules)
                 {
-                    return false;
+                    if (line.Contains('|'))
+                    {
+                        var parts = line.Split('|');
+                        if (parts.Length != 2)
+                            throw new FormatException($"Invalid rule format: {line}");
+
+                        int before = int.Parse(parts[0].Trim());
+                        int after = int.Parse(parts[1].Trim());
+
+                        if (!rules.ContainsKey(before)) rules[before] = new HashSet<int>();
+                        rules[before].Add(after);
+                    }
+                    else
+                    {
+                        parsingRules = false;
+                    }
+                }
+
+                if (!parsingRules)
+                {
+                    if (line.Contains(','))
+                    {
+                        updates.Add(line.Split(',')
+                            .Select(x => int.Parse(x.Trim()))
+                            .ToList());
+                    }
+                    else
+                    {
+                        throw new FormatException($"Invalid update format: {line}");
+                    }
                 }
             }
+            return (rules, updates);
         }
-        return true;
+        
+        private static int SumMiddlePages(Dictionary<int, HashSet<int>> rules, List<List<int>> updates)
+        {
+            int sum = 0;
+
+            foreach (var update in updates)
+            {
+                if (IsCorrectOrder(rules, update))
+                {
+                    int middleIndex = update.Count / 2;
+                    sum += update[middleIndex];
+                }
+            }
+
+            return sum;
+        }
+
+        private static bool IsCorrectOrder(Dictionary<int, HashSet<int>> rules, List<int> update)
+        {
+            for (int i = 0; i < update.Count; i++)
+            {
+                for (int j = i + 1; j < update.Count; j++)
+                {
+                    if (rules.ContainsKey(update[j]) && rules[update[j]].Contains(update[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
